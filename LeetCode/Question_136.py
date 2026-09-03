baa1e795c40d2d16736f97481014778ebf25fe2c
Deepak Rajak <dr2317937@gmail.com>
2026-09-03 14:43:54 +0530

    Added solution for Question 136

--- LeetCode/Question_136.py
+++ LeetCode/Question_136.py
@@ -1,1 +1,20 @@
-'''Given a non-empty integer array nums where every element appears twice except for one element, write a Python program to find the element that appears only once. The solution must have O(n) time complexity and O(1) extra space.'''
+'''
+Question:
+Given a non-empty integer array nums where every element appears twice
+except for one element, find the element that appears only once.
+The solution must have O(n) time complexity and O(1) extra space.
+'''
+
+class Solution(object):
+
+    def singleNumber(self, nums):
+
+        # Initialize result
+        ans = 0
+
+        # XOR all elements
+        for num in nums:
+            ans = ans ^ num
+
+        # Return the single number
+        return ans
